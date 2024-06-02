1237aa1ebb04e7262a99433ae6038ae17c725a2c
xaiphyr <xaiphyrrr@gmail.com>
2024-06-02 18:16:04 +0800

    chore: installed go-chi package, updated main.go, added develop watch for docker-compose.yml

--- src/backend/go.mod
+++ src/backend/go.mod
@@ -4,7 +4,11 @@ go 1.22.2
 
 require github.com/rubenv/sql-migrate v1.6.1
 
+require github.com/cespare/xxhash/v2 v2.1.2 // indirect
+
 require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/httprate v0.9.0
 	github.com/go-gorp/gorp/v3 v3.1.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/stretchr/testify v1.8.1 // indirect
--- src/backend/main.go
+++ src/backend/main.go
@@ -3,16 +3,51 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"time"
 
 	sql "app_backend/sql"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/httprate"
 
 	_ "github.com/lib/pq"
 )
 
+var endpoint = os.Getenv("ENDPOINT")
+var version = os.Getenv("VERSION")
+var url = func(api string) string {
+	return endpoint + api
+}
+
 func main() {
-	fmt.Println("MAIN INITIALIZE...")
-
+	port := os.Getenv("HTTP_PORT")
 	log.SetFlags(log.Llongfile | log.LstdFlags)
-
 	sql.MigrateSQL()
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.GetHead)
+	r.Use(httprate.LimitByIP(100, 1*time.Minute))
+
+	r.Get(fmt.Sprintf("%s/", endpoint), func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("WELCOME!"))
+	})
+
+	r.Route(url("/hello"), func(r chi.Router) {
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("HELLO WORLD"))
+		})
+	})
+
+	fmt.Println()
+	log.Printf("-> Version: %s", version)
+	log.Printf("-> Local:   http://localhost:8200")
+
+	http.ListenAndServe(fmt.Sprintf(":%s", port), r)
 }
